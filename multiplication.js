738c212772550a1c31beeb1e5a3436d73c604340
Saurabh singh <ajinkyabisht75@gmail.com>
2025-11-02 22:02:58 +0530

    Day 5 multiplication file

---
+++ multiplication.js
@@ -0,0 +1,12 @@
+// write a prog the print the table of given number
+
+function multiplication(Nums, n){
+    if(typeof(Nums) !==Number){
+        throw new Error("only Integer are allowed");
+    }else{ 
+        for(let i = 1; i <= n; i++){
+        console.log(`${Nums} * ${i} = ${Nums*i}`);
+        }
+    }
+}
+console.log(multiplication(3,10));
