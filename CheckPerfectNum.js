84bc5c4222884e60b1e72f95b4a97d6d19d34fa3
Saurabh singh <ajinkyabisht75@gmail.com>
2025-11-09 19:38:30 +0530

    Day 11 checking the perfect number

---
+++ CheckPerfectNum.js
@@ -0,0 +1,18 @@
+// write a program that checks if a given number is a perfect number
+
+function CheckPerfectNums(InputNumber){
+    let result = 0;
+    for (let i = 0; i <= InputNumber/2; i++){
+        if(InputNumber % i == 0){
+            result = result + i;
+        }
+    }
+    if(result === InputNumber){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+const number = 496;
+console.log(CheckPerfectNums(number));
